7b0a62e5a53dd7bb185920c1795d52e5502313a6
Joe Oliveri <oliverijoe1@gmail.com>
2022-02-04 13:25:03 -0500

    Working on function to change colors for each box

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-const box1 = document.getElementById("box1");
+
 const gridBox = document.querySelector(".grid-box");
 const resetButton = document.querySelector(".reset-button");
+
 
 // Function that cycles through the colors
 
 const changeColor = function() {
-    if (gridBox.classList.contains("white")) {
-        gridBox.classList.remove("white");
-        gridBox.classList.add("blue");
-    } else if (gridBox.classList.contains("blue")) {
-        gridBox.classList.remove("blue");
-        gridBox.classList.add("red");
+    if (box.classList.contains("white")) {
+        box.classList.remove("white");
+        box.classList.add("blue");
+    } else if (box.classList.contains("blue")) {
+        box.classList.remove("blue");
+        box.classList.add("red");
+    } else if (box.classList.contains("red")) {
+        box.classList.remove("red");
+        box.classList.add("green");
+    } else if (box.classList.contains("green")) {
+        box.classList.remove("green");
+        box.classList.add("yellow");
+    } else if (box.classList.contains("yellow")) {
+        box.classList.remove("yellow");
+        box.classList.add("orange");
     }
 };
 
-
-
 // Click a box on the grid and cycle through the colors
 
-box1.addEventListener("click", changeColor());
+box.addEventListener("click", changeColor);
+
+// Reset Button 
+
+resetButton.addEventListener("click", function () {
+    console.log("Hi");
+//    for (let i = 1; i <= 25; i++) {
+//        let num = i;
+//        document.getElementById(`"box-${num}"`).classList.replace("white", "blue");
+//    }
+});
