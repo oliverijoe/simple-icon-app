0517b92d6743907c7e4998a73be6ee5dfa9cb405
Joe Oliveri <oliverijoe1@gmail.com>
2022-02-05 08:56:57 -0500

    JS is functioning but only turning boxes blue; need to work on reset button as well

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-
-const gridBox = document.querySelector(".grid-box");
-const resetButton = document.querySelector(".reset-button");
+const box_1 = document.getElementById("box-1");
+const box_2 = document.getElementById("box-2");
+const box_3 = document.getElementById("box-3");
+const box_4 = document.getElementById("box-4");
+const box_5 = document.getElementById("box-5");
+const box_6 = document.getElementById("box-6");
+const box_7 = document.getElementById("box-7");
+const box_8 = document.getElementById("box-8");
+const box_9 = document.getElementById("box-9");
+const box_10 = document.getElementById("box-10");
+const box_11 = document.getElementById("box-11");
+const box_12 = document.getElementById("box-12");
+const box_13 = document.getElementById("box-13");
+const box_14 = document.getElementById("box-14");
+const box_15 = document.getElementById("box-15");
+const box_16 = document.getElementById("box-16");
+const box_17 = document.getElementById("box-17");
+const box_18 = document.getElementById("box-18");
+const box_19 = document.getElementById("box-19");
+const box_20 = document.getElementById("box-20");
+const box_21 = document.getElementById("box-21");
+const box_22 = document.getElementById("box-22");
+const box_23 = document.getElementById("box-23");
+const box_24 = document.getElementById("box-24");
+const box_25 = document.getElementById("box-25");
+const allBoxes = document.querySelectorAll(".grid-box");
+const resetButton = document.getElementsByClassName(".reset-button");
 
 
 // Function that cycles through the colors
 
-const changeColor = function() {
+const changeColor = function(e) {
+    const box = e.target;
     if (box.classList.contains("white")) {
         box.classList.remove("white");
         box.classList.add("blue");
@@ -21,19 +46,48 @@ const changeColor = function() {
     } else if (box.classList.contains("yellow")) {
         box.classList.remove("yellow");
         box.classList.add("orange");
-    }
+    } else
+        box.classList.remove("orange");
+        box.classList.add("white");
 };
 
-// Click a box on the grid and cycle through the colors
+// Listens for a click on a particular box and runs through the above funcition
+// to change the color of the box
 
-box.addEventListener("click", changeColor);
+box_1.addEventListener("click", changeColor);
+box_2.addEventListener("click", changeColor);
+box_3.addEventListener("click", changeColor);
+box_4.addEventListener("click", changeColor);
+box_5.addEventListener("click", changeColor);
+box_6.addEventListener("click", changeColor);
+box_7.addEventListener("click", changeColor);
+box_8.addEventListener("click", changeColor);
+box_9.addEventListener("click", changeColor);
+box_10.addEventListener("click", changeColor);
+box_11.addEventListener("click", changeColor);
+box_12.addEventListener("click", changeColor);
+box_13.addEventListener("click", changeColor);
+box_14.addEventListener("click", changeColor);
+box_15.addEventListener("click", changeColor);
+box_16.addEventListener("click", changeColor);
+box_17.addEventListener("click", changeColor);
+box_18.addEventListener("click", changeColor);
+box_19.addEventListener("click", changeColor);
+box_20.addEventListener("click", changeColor);
+box_21.addEventListener("click", changeColor);
+box_22.addEventListener("click", changeColor);
+box_23.addEventListener("click", changeColor);
+box_24.addEventListener("click", changeColor);
+box_24.addEventListener("click", changeColor);
+box_25.addEventListener("click", changeColor);
 
-// Reset Button 
+// Reset Button - resets all boxes to white
 
 resetButton.addEventListener("click", function () {
-    console.log("Hi");
-//    for (let i = 1; i <= 25; i++) {
-//        let num = i;
-//        document.getElementById(`"box-${num}"`).classList.replace("white", "blue");
-//    }
+    allBoxes.classList.remove("blue");
+    allBoxes.classList.remove("green");
+    allBoxes.classList.remove("red");
+    allBoxes.classList.remove("yellow");
+    allBoxes.classList.remove("orange");
+    allBoxes.classList.add("white");
 });
